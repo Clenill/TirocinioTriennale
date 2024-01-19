60ee3c39b80a404f826d0abba75135682f364f86
Clenill <jokerforpeace@gmail.com>
2024-01-19 11:18:55 +0100

    Modificato error controller con autorità e modificato header principale con ruoli

--- src/main/java/com/tirociniotriennale/sitoeventi/controller/MyErrorController.java
+++ src/main/java/com/tirociniotriennale/sitoeventi/controller/MyErrorController.java
@@ -1,21 +1,61 @@
 package com.tirociniotriennale.sitoeventi.controller;
 
 import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
 
 @Controller
 public class MyErrorController implements ErrorController{
 
     @RequestMapping("/error")
     public String gestioneErrori(Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        //Verifico che l'utente è autenticato
+        if (authentication.isAuthenticated()){
+            Object principal = authentication.getPrincipal();
+            // casting del principal se è un'istanza di UserDetails
+            if(principal instanceof UserDetails){
+                UserDetails userDetails = (UserDetails) principal;
+                String username = userDetails.getUsername();
+                Collection<? extends GrantedAuthority> collectionautorita = userDetails.getAuthorities();
+
+                String primaAut = collectionautorita.iterator().next().getAuthority();
+                model.addAttribute("autorita", primaAut);
+
+                model.addAttribute("nomeutente", username);
+
+            }
+        }
         model.addAttribute("validationErrors", "");
         return "error";
     }
 
     @RequestMapping("/403")
-    public String accessDenied() {
+    public String accessDenied(Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        //Verifico che l'utente è autenticato
+        if (authentication.isAuthenticated()){
+            Object principal = authentication.getPrincipal();
+            // casting del principal se è un'istanza di UserDetails
+            if(principal instanceof UserDetails){
+                UserDetails userDetails = (UserDetails) principal;
+                String username = userDetails.getUsername();
+                Collection<? extends GrantedAuthority> collectionautorita = userDetails.getAuthorities();
+
+                String primaAut = collectionautorita.iterator().next().getAuthority();
+                model.addAttribute("autorita", primaAut);
+
+                model.addAttribute("nomeutente", username);
+
+            }
+        }
         return "403";
     }
 }
